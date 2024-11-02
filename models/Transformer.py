85254854ff6db643b27c84bb5a73630573a1c5e8
Vinmwaura <vinmwega@gmail.com>
2024-11-02 16:27:57 +0300

    Tweaked Transformer model to include new changes for the architectural changes

--- models/Transformer.py
+++ models/Transformer.py
@@ -6,55 +6,84 @@ from .layers import (
     TransformerBlock,
     get_positional_embeddings)
 
-
 """
-Transformer architecture: https://arxiv.org/abs/1706.03762
+Vanilla Transformer + DiT architecture:
+> https://arxiv.org/abs/1706.03762
+> https://arxiv.org/abs/2212.09748
 """
 class Transformer(nn.Module):
     def __init__(
             self,
-            num_enc_embedding,
-            num_dec_embedding,
-            num_enc_layers,
-            num_dec_layers,
-            self_attn_heads,
-            cross_attn_heads,
-            transformer_in_dim,
-            transformer_out_dim,
-            transformer_hidden_dim,
+            use_encoder=True,
+            use_pos_cond=True,
+            num_enc_layers=5,
+            num_dec_layers=10,
+            num_enc_embedding=512,
+            num_dec_embedding=512,
+            self_attn_heads=8,
+            cross_attn_heads=8,
+            transformer_in_dim=512,
+            transformer_out_dim=512,
+            transformer_hidden_dim=4096,
             hidden_activation="silu"):
         super().__init__()
 
-        self.enc_embedding = nn.Embedding(
-            num_embeddings=num_enc_embedding,
-            embedding_dim=transformer_in_dim)
+        self.use_encoder = use_encoder
+        self.use_pos_cond = use_pos_cond
+
+        if self.use_encoder:
+            self.enc_embedding = nn.Embedding(
+                num_embeddings=num_enc_embedding,
+                embedding_dim=transformer_in_dim)
+
+            # Vanilla Transformer Architecture.
+            self.encoder_layers = nn.ModuleList()
+            for _ in range(num_enc_layers):
+                self.encoder_layers.append(
+                    TransformerBlock(
+                        in_dim=transformer_in_dim,
+                        hidden_dim=transformer_hidden_dim,
+                        self_attn_heads=self_attn_heads,
+                        use_cross_attn=False,
+                        use_masked_attn=False,
+                        use_adaln0=False,
+                        use_scale_layer=False,
+                        activation_type=hidden_activation))
+
         self.dec_embedding = nn.Embedding(
-            num_embeddings=num_dec_embedding + 1,
+            num_embeddings=num_dec_embedding + 1,  # Includes <Start> Token.
             embedding_dim=transformer_in_dim)
 
-        self.encoder_layers = nn.ModuleList()
-        for _ in range(num_enc_layers):
-            self.encoder_layers.append(
-                TransformerBlock(
-                    self_attn_heads=self_attn_heads,
-                    in_dim=transformer_in_dim,
-                    hidden_dim=transformer_hidden_dim,
-                    use_cross_attn=False,
-                    use_masked_attn=False,
-                    activation_type=hidden_activation))
-
+        # Diffusion Transformer Architecture.
         self.decoder_layers = nn.ModuleList()
         for _ in range(num_dec_layers):
             self.decoder_layers.append(
                 TransformerBlock(
-                    self_attn_heads=self_attn_heads,
-                    cross_attn_heads=cross_attn_heads,
                     in_dim=transformer_in_dim,
                     cond_dim=transformer_in_dim,
+                    cross_cond_dim=transformer_in_dim,
                     hidden_dim=transformer_hidden_dim,
+                    self_attn_heads=self_attn_heads,
+                    cross_attn_heads=cross_attn_heads,
                     use_cross_attn=True,
                     use_masked_attn=True,
+                    use_adaln0=self.use_pos_cond,
+                    use_scale_layer=self.use_pos_cond,
                     activation_type=hidden_activation))
+
+        # MLP for Positional conditioning.
+        # If using sliding window, pass position of patches as condition.
+        if self.use_pos_cond:
+            self.pos_cond_layer = nn.Sequential(
+                LinearLayer(
+                    in_dim=transformer_in_dim,
+                    out_dim=transformer_hidden_dim,
+                    use_activation=True,
+                    activation_type=hidden_activation),
+                LinearLayer(
+                    in_dim=transformer_hidden_dim,
+                    out_dim=transformer_in_dim,
+                    use_activation=False))
 
         self.classifier = nn.Sequential(
             LinearLayer(
@@ -84,48 +113,62 @@ class Transformer(nn.Module):
 
             own_state[name].copy_(param)
 
-    def forward(self, x, cond):
-        """
-        Encoder Half.
-        """
-        x_enc = self.enc_embedding(cond)
+    def forward(self, x_dec, x_enc=None, pos_cond=None):
+        if self.use_encoder:
+            """
+            Encoder Half.
+            """
+            x_enc = self.enc_embedding(x_enc)
 
-        _, enc_Seq, D = x_enc.shape
-        enc_pos_index = torch.arange(
-            start=1,
-            end=enc_Seq + 1,
-            device=cond.device)  # (enc_Seq,)
-        enc_pos_emb = get_positional_embeddings(
-            emb_dim=D,
-            pos_index=enc_pos_index).unsqueeze(dim=0)  # (1, enc_Seq, D)
+            _, enc_Seq, enc_D = x_enc.shape
+            enc_pos_index = torch.arange(
+                start=1,
+                end=enc_Seq + 1,
+                device=x_enc.device)  # (enc_Seq,)
+            enc_pos_emb = get_positional_embeddings(
+                emb_dim=enc_D,
+                pos_index=enc_pos_index).unsqueeze(dim=0)  # (1, enc_Seq, D)
 
-        # Adds Positional to the input.
-        x_enc = x_enc + enc_pos_emb
+            # Adds Positional to the input.
+            x_enc = x_enc + enc_pos_emb
 
-        for encoder_layer in self.encoder_layers:
-            x_enc = encoder_layer(x_enc)
+            for encoder_layer in self.encoder_layers:
+                x_enc = encoder_layer(x_enc)
 
         """
         Decoder Half.
         """
-        x_dec = self.dec_embedding(x)
+        # Output Embedding.
+        x_dec = self.dec_embedding(x_dec)  # (N, Seq, D)
 
-        _, dec_Seq, _ = x_dec.shape
+        dec_N, dec_Seq, dec_D = x_dec.shape
+
+        # Positional Encoding.
         dec_pos_index = torch.arange(
             start=1,
             end=dec_Seq + 1,
-            device=x.device)  # (enc_Seq,)
+            device=x_dec.device)  # (enc_Seq,)
         dec_pos_emb = get_positional_embeddings(
-            emb_dim=D,
+            emb_dim=dec_D,
             pos_index=dec_pos_index).unsqueeze(dim=0)  # (1, dec_Seq, D)
 
-        # Adds Positional to the input.
         x_dec = x_dec + dec_pos_emb
+
+        # Patch Position Conditioning.
+        pos_cond_emb = None
+        if self.use_pos_cond:
+            pos_cond_flat = pos_cond.flatten()  # (N*Seq,)
+            pos_cond_emb = get_positional_embeddings(
+                emb_dim=dec_D,
+                pos_index=pos_cond_flat).unsqueeze(dim=0)  # (N*Seq, D)
+            pos_cond_emb = pos_cond_emb.reshape(dec_N, dec_Seq, dec_D)  # (N, Seq, D)
+            pos_cond_emb = self.pos_cond_layer(pos_cond_emb)  # (N, Seq, D)
 
         for decoder_layer in self.decoder_layers:
             x_dec = decoder_layer(
                 x=x_dec,
-                cross_cond=x_enc)
+                cross_cond=x_enc,
+                pos_cond=pos_cond_emb)
 
         x_class = self.classifier(x_dec)
         return x_class
