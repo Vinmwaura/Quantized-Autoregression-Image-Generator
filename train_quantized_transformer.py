b2b98b2fa640b481c855bcdb9c4dd00055d5d7af
Vinmwaura <vinmwega@gmail.com>
2024-11-02 18:57:09 +0300

    Modified code for training transformer to work with the architectural changes.

--- train_quantized_transformer.py
+++ train_quantized_transformer.py
@@ -30,7 +30,7 @@ def main():
 
     parser = argparse.ArgumentParser(
         description=f"Train {project_name} models.")
-    
+
     parser.add_argument(
         "--device",
         help="Which hardware device will model run on.",
@@ -50,7 +50,8 @@ def main():
     parser.add_argument(
         "--lr-codebook-path",
         help="File path to saved Low-Res codebook.",
-        required=True,
+        default=None,
+        required=False,
         type=pathlib.Path)
     parser.add_argument(
         "--hr-codebook-path",
@@ -144,9 +145,7 @@ def main():
     curr_epoch = 0
     global_steps = 0
 
-    """
-    Pre-trained Decoder model.
-    """
+    # Pre-trained Decoder model.
     decoder_model_status, decoder_model_dict = load_model(decoder_path)
     if not decoder_model_status:
         raise Exception("An error occured while loading decoder model checkpoint!")
@@ -171,31 +170,29 @@ def main():
         final_activation_type=dec_final_activation_type).to(device)
     decoder_model.custom_load_state_dict(decoder_model_dict["model"])
 
-    """
-    Low - Resolution Codebook.
-    """
-    lr_codebook_status, lr_codebook_dict = load_model(lr_codebook_path)
-    if not lr_codebook_status:
-        raise Exception("An error occured while loading Low-Resolution codebook checkpoint!")
+    # Low - Resolution Codebook (If any passed).
+    lr_codebook = None
+    if lr_codebook_path is not None:
+        lr_codebook_status, lr_codebook_dict = load_model(lr_codebook_path)
+        if not lr_codebook_status:
+            raise Exception("An error occured while loading Low-Resolution codebook checkpoint!")
 
-    lr_patch_dim = lr_codebook_dict["patch_dim"]
-    lr_num_embeddings = lr_codebook_dict["num_embeddings"]
-    lr_image_dim = lr_codebook_dict["image_dim"]
-    lr_image_C = lr_codebook_dict["image_C"]
-    lr_neighbourhood_range = lr_codebook_dict["neighbourhood_range"]
+        lr_patch_dim = lr_codebook_dict["patch_dim"]
+        lr_num_embeddings = lr_codebook_dict["num_embeddings"]
+        lr_image_dim = lr_codebook_dict["image_dim"]
+        lr_image_C = lr_codebook_dict["image_C"]
+        lr_neighbourhood_range = lr_codebook_dict["neighbourhood_range"]
 
-    # Initialize Low-Resolution Codebook.
-    lr_codebook = Codebook(
-        patch_dim=lr_patch_dim,
-        image_dim=lr_image_dim,
-        image_channel=lr_image_C,
-        num_embeddings=lr_num_embeddings,
-        init_neighbour_range=lr_neighbourhood_range).to(device)
-    lr_codebook.custom_load_state_dict(lr_codebook_dict["checkpoint"])
+        # Initialize Low-Resolution Codebook.
+        lr_codebook = Codebook(
+            patch_dim=lr_patch_dim,
+            image_dim=lr_image_dim,
+            image_channel=lr_image_C,
+            num_embeddings=lr_num_embeddings,
+            init_neighbour_range=lr_neighbourhood_range).to(device)
+        lr_codebook.custom_load_state_dict(lr_codebook_dict["checkpoint"])
 
-    """
-    High - Resolution Codebook (If Any).
-    """
+    # High - Resolution Codebook (If Any).
     hr_codebook_status, hr_codebook_dict = load_model(hr_codebook_path)
     if not hr_codebook_status:
         raise Exception("An error occured while loading Low-Resolution codebook checkpoint!")
@@ -215,11 +212,22 @@ def main():
         init_neighbour_range=hr_neighbourhood_range).to(device)
     hr_codebook.custom_load_state_dict(hr_codebook_dict["checkpoint"])
 
-    # Model Params.
+    # Quantized Transformer Model Params.
     model_lr = config_dict["model_lr"]
-    num_enc_embedding = lr_num_embeddings
+    use_encoder = config_dict["use_encoder"]
+    if use_encoder:
+        num_enc_embedding = lr_num_embeddings
+        num_enc_layers = config_dict["num_enc_layers"]
+    else:
+        num_enc_embedding = None
+        num_enc_layers = None
+    use_sliding_window = config_dict["use_sliding_window"]
+
+    sliding_window = None
+    if use_sliding_window:
+        sliding_window = config_dict["sliding_window"]
+
     num_dec_embedding = hr_num_embeddings
-    num_enc_layers = config_dict["num_enc_layers"]
     num_dec_layers = config_dict["num_dec_layers"]
     self_attn_heads = config_dict["self_attn_heads"]
     cross_attn_heads = config_dict["cross_attn_heads"]
@@ -228,14 +236,14 @@ def main():
     transformer_hidden_dim = config_dict["hidden_dim"]
     hidden_activation = config_dict["hidden_activation"]
 
-    """
-    Transformer model, to be trained.
-    """
+    # Transformer model, to be trained.
     model = Transformer(
-        num_enc_embedding=num_enc_embedding,
-        num_dec_embedding=num_dec_embedding,
+        use_encoder=use_encoder,
+        use_pos_cond=use_sliding_window,
         num_enc_layers=num_enc_layers,
         num_dec_layers=num_dec_layers,
+        num_enc_embedding=num_enc_embedding,
+        num_dec_embedding=num_dec_embedding,
         self_attn_heads=self_attn_heads,
         cross_attn_heads=cross_attn_heads,
         transformer_in_dim=transformer_in_dim,
@@ -276,11 +284,12 @@ def main():
         batch_size=batch_size,
         num_workers=4,
         shuffle=True)
-    test_feature_map_dataloader = torch.utils.data.DataLoader(
-        feature_map_dataset,
-        batch_size=test_num_sample,
-        num_workers=4,
-        shuffle=True)
+    if use_encoder:
+        test_feature_map_dataloader = torch.utils.data.DataLoader(
+            feature_map_dataset,
+            batch_size=test_num_sample,
+            num_workers=4,
+            shuffle=True)
 
     model_params_size = sum(param.numel() for param in model.parameters())
 
@@ -298,15 +307,19 @@ def main():
     logging.info(f"Decoder activation type: {dec_final_activation_type}")
     logging.info("#" * 100)
     logging.info(f"Codebook Parameters.")
-    logging.info(f"Low Res Patch size: {lr_patch_dim}")
+    if lr_codebook is not None:
+        logging.info(f"Low Res Patch size: {lr_patch_dim}")
+        logging.info(f"Low Res Num Embeddings: {lr_num_embeddings:,}")
     logging.info(f"High Res Patch size: {hr_patch_dim}")
-    logging.info(f"Low Res Num Embeddings: {lr_num_embeddings:,}")
     logging.info(f"High Res Num Embeddings: {hr_num_embeddings:,}")
     logging.info("#" * 100)
     logging.info(f"Transformer Parameters.")
-    logging.info(f"Num Encoder Embedding: {num_enc_embedding:,}")
+    if use_sliding_window:
+        logging.info(f"Sliding Window: {sliding_window:,}")
+    if use_encoder:
+        logging.info(f"Num Encoder Embedding: {num_enc_embedding:,}")
+        logging.info(f"Num Encoder Layers: {num_enc_layers:,}")
     logging.info(f"Num Decoder Embedding: {num_dec_embedding:,}")
-    logging.info(f"Num Encoder Layers: {num_enc_layers:,}")
     logging.info(f"Num Decoder Layers: {num_dec_layers:,}")
     logging.info(f"Self Attention Heads: {self_attn_heads:,}")
     logging.info(f"Cross Attention Heads: {cross_attn_heads:,}")
@@ -331,37 +344,73 @@ def main():
 
             feature_map = feature_map.to(device)
 
-            lr_codebook.eval()
-            hr_codebook.eval()
+            lr_input = None
+            if lr_codebook is not None:
+                lr_codebook.eval()
+                lr_input = lr_codebook.get_patches_bmu(
+                    feature_map,
+                    reshape=True)  # (N, lr_Seq)
 
-            lr_input = lr_codebook.get_patches_bmu(
-                feature_map,
-                reshape=True)
+            hr_codebook.eval()
             hr_indices = hr_codebook.get_patches_bmu(
                 feature_map,
-                reshape=True)
+                reshape=True)  # (N, hr_Seq)
 
-            N, _ = hr_indices.shape
+            N, total_Seq = hr_indices.shape
 
             # Add <start> token.
             start_tensor = torch.tensor(
                 [[hr_num_embeddings]],
                 device=device).repeat(N, 1)
-            hr_input = torch.cat((start_tensor, hr_indices), dim=1)
+            hr_input = torch.cat(
+                (start_tensor,hr_indices),
+                dim=1)
+            hr_input = hr_input.to(device)
 
             # Add <end> token.
             end_tensor = torch.tensor(
                 [[hr_num_embeddings]],
                 device=device).repeat(N, 1)
-            hr_target = torch.cat((hr_indices, end_tensor), dim=1)
+            hr_target = torch.cat(
+                (hr_indices, end_tensor),
+                dim=1)
+            hr_target = hr_target.to(device)
+
+            sliding_window_indices = None
+            if use_sliding_window:
+                # Generate all the possible sections of the sliding window.
+                hr_input_unfold = hr_input.unfold(
+                    dimension=1,
+                    size=sliding_window,
+                    step=1)
+                hr_target_unfold = hr_target.unfold(
+                    dimension=1,
+                    size=sliding_window,
+                    step=1)
+
+                _, num_sliding_windows, _ = hr_input_unfold.shape
+
+                # Randomly pick a section of the data using a sliding window. 
+                rand_indices = torch.randint(
+                    low=0,
+                    high=num_sliding_windows,
+                    size=(N,))
+                hr_input = hr_input_unfold[torch.arange(N), rand_indices, :]  # (N, D)
+                hr_target = hr_target_unfold[torch.arange(N), rand_indices, :]  # (N, D)
+
+                sliding_window_indices = rand_indices.unsqueeze(dim=1) +\
+                 torch.arange(sliding_window).unsqueeze(dim=0)  # (N, Window)
+
+                sliding_window_indices = sliding_window_indices.to(device)
 
             model.train()
 
             model_optim.zero_grad()
 
             classification_out = model(
-                x=hr_input,
-                cond=lr_input)
+                x_dec=hr_input,
+                x_enc=lr_input,
+                pos_cond=sliding_window_indices)
 
             _, Seq, C = classification_out.shape            
             out_seq_flat = classification_out.view(N*Seq, C)  # (N*Seq, Class)
@@ -388,6 +437,9 @@ def main():
             if global_steps % model_checkpoint_step == 0 and global_steps >= 0:
                 # Save diffusion models, and optimizers.
                 model_dict = {
+                    "use_encoder": use_encoder,
+                    "use_sliding_window": use_sliding_window,
+                    "sliding_window": sliding_window,
                     "num_enc_embedding": num_enc_embedding,
                     "num_dec_embedding": num_dec_embedding,
                     "num_enc_layers": num_enc_layers,
@@ -410,7 +462,7 @@ def main():
                     logging.info("Successfully saved model.")
                 else:
                     logging.info("Error occured saving model.")
-                
+
                 # Autoregressively generate image one token at a time.
                 """
                 Lower Temperature (T < 1):
@@ -425,34 +477,87 @@ def main():
                 """
                 temperature = 1.0  # Hardcoded to 1 for now.
 
+                if lr_codebook is not None:
+                    lr_codebook.eval()
+
                 model.eval()
-                lr_codebook.eval()
                 hr_codebook.eval()
                 decoder_model.eval()
-                
-                with torch.no_grad():
-                    test_feature_map = next(iter(test_feature_map_dataloader))
-                    test_feature_map = test_feature_map.to(device)
 
-                    test_lr_input = lr_codebook.get_patches_bmu(
-                        test_feature_map,
-                        reshape=True)
-                    test_hr_input_example = hr_codebook.get_patches_bmu(
-                        test_feature_map,
-                        reshape=True)
+                with torch.no_grad():
+                    test_lr_input = None
+                    test_hr_input_example = None
+
+                    if use_encoder:
+                        test_feature_map = next(iter(test_feature_map_dataloader))
+                        test_feature_map = test_feature_map.to(device)
+
+                        test_lr_input = lr_codebook.get_patches_bmu(
+                            test_feature_map,
+                            reshape=True)
+
+                        latent_decoder = decoder_model(test_feature_map)
+
+                        test_hr_quant = hr_codebook(test_feature_map)
+                        test_lr_quant = lr_codebook(test_feature_map)
+
+                        hr_decoder = decoder_model(test_hr_quant)
+                        lr_decoder = decoder_model(test_lr_quant)
+
+                        # Save Images.
+                        _ = save_images(
+                            images=latent_decoder,
+                            file_name=f"ground_truth_{global_steps}",
+                            dest_path=out_dir,
+                            logging=logging.info)
+                        _ = save_images(
+                            images=lr_decoder,
+                            file_name=f"low_res_cond_{global_steps}",
+                            dest_path=out_dir,
+                            logging=logging.info)
+                        _ = save_images(
+                            images=hr_decoder,
+                            file_name=f"high_res_example_{global_steps}",
+                            dest_path=out_dir,
+                            logging=logging.info)
+
+                        test_lr_indices = lr_codebook.get_patches_bmu(
+                            test_feature_map,
+                            reshape=True)  # (N, Seq)
+                        test_hr_indices = hr_codebook.get_patches_bmu(
+                            test_feature_map,
+                            reshape=True)  # (N, Seq)
+
+                    start_index = 0
 
                     # Add <start> token.
                     test_hr_input = torch.tensor(
                         [[hr_num_embeddings]],
-                        device=device).repeat(test_num_sample, 1)
+                        device=device).repeat(test_num_sample,1)  # (test_N,1)
 
-                    _, hr_Seq = test_hr_input_example.shape
+                    test_pos_indices = None
+                    if use_sliding_window:
+                        # Initial Position indices, starts at 0.
+                        test_pos_indices = torch.zeros(
+                            (test_num_sample,1),
+                            device=device)  # (test_N,1)
 
                     # TODO: Implement a Beam Search to improve output quality.
-                    for step in range(hr_Seq):
+                    for step in range(total_Seq):
+                        print(f"{step:,} / {total_Seq:,}")
+
+                        _, temp_Seq = test_hr_input.shape
+                        if temp_Seq >= sliding_window:
+                            start_index = start_index + 1
+
+                            test_pos_indices = test_pos_indices[:, 1:]
+
                         out_seq = model(
-                            x=test_hr_input,
-                            cond=test_lr_input)  # (N, Seq, Class)
+                            x_dec=test_hr_input[:,start_index:],
+                            x_enc=test_lr_input,
+                            pos_cond=test_pos_indices)
+
+                        # Take the last prediction as the next-token.
                         out_seq = out_seq[:, -1, :]  # (N, Class)
 
                         probs = F.softmax(out_seq / temperature, dim=1)  # (N, Class)
@@ -460,7 +565,16 @@ def main():
                         # Pick most likely token for next generation for each Token Sequence (Seq).
                         next_token = torch.multinomial(probs, 1)
 
-                        test_hr_input = torch.cat((test_hr_input, next_token), dim=1)
+                        test_hr_input = torch.cat(
+                            (test_hr_input,next_token),
+                            dim=1)
+                        if use_sliding_window:
+                            temp_indices = torch.tensor(
+                                [[step + 1]],
+                                device=device).repeat(test_num_sample, 1)
+                            test_pos_indices = torch.cat(
+                                (test_pos_indices, temp_indices),
+                                dim=1)
 
                     test_hr_input = test_hr_input[:,1:]
                     test_hr_input[test_hr_input==hr_num_embeddings] = 0
@@ -468,42 +582,18 @@ def main():
                     test_hr_quant = hr_codebook.get_quantized_image(
                         indices=test_hr_input,
                         unpatchify_input=True)
-                    test_lr_quant = lr_codebook.get_quantized_image(
-                        indices=test_lr_input,
-                        unpatchify_input=True)
-                    test_hr_quant_example = hr_codebook.get_quantized_image(
-                        indices=test_hr_input_example,
-                        unpatchify_input=True)
-
-                    latent_decoder = decoder_model(test_feature_map)
-                    lr_decoder = decoder_model(test_lr_quant)
-                    hr_example_decoder = decoder_model(test_hr_quant_example)
                     hr_recon_decoder = decoder_model(test_hr_quant)
 
                     # Save Images.
-                    _ = save_images(
-                        images=latent_decoder,
-                        file_name=f"ground_truth_{global_steps}",
-                        dest_path=out_dir,
-                        logging=logging.info)
-                    _ = save_images(
-                        images=lr_decoder,
-                        file_name=f"low_res_cond_{global_steps}",
-                        dest_path=out_dir,
-                        logging=logging.info)
                     _ = save_images(
                         images=hr_recon_decoder,
                         file_name=f"high_res_recon_{global_steps}",
                         dest_path=out_dir,
                         logging=logging.info)
-                    _ = save_images(
-                        images=hr_example_decoder,
-                        file_name=f"high_res_example_{global_steps}",
-                        dest_path=out_dir,
-                        logging=logging.info)
 
-                del test_hr_input
-                torch.cuda.empty_cache()
+                    del test_hr_input
+                    del test_pos_indices
+                    torch.cuda.empty_cache()
 
             temp_avg_loss = total_loss / iteration_count
             message = "Cum. Steps: {:,} | Steps: {:,} / {:,} | L.R.: {:.8f} | Recon Loss: {:.5f}".format(
